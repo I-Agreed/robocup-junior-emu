929f1c638ca7ef7112d9193a1350d2182c6ef5c2
chickensalt <nsdj.sharma@gmail.com>
2021-04-16 18:48:53 +1000

    better variable names, add todos

--- vision.py
+++ vision.py
@@ -8,16 +8,19 @@ class Vision:
         self,
         yellowGoalRange: Tuple[List[int]],
         blueGoalRange: Tuple[List[int]],
+        #TODO: ^- Add default values for these
         ballRange: Tuple[List[int]] = ([14, 29, 178], [43, 169, 255]),
-        #TODO: ^- Change these to actual color of competition ball, not a tennis ball (also use the webcam camera)
+        #TODO: ^- Change these to actual color of competition ball, not a tennis ball
         camHeight: int = 150,
         vidSource: int = 0,
     ):
         self.cap = cv2.VideoCapture(vidSource)
         self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 100)
         self.ballRange = (np.array(ballRange[0]), np.array(ballRange[1]))
-        self.yellowGoalRange = (np.array(yellowGoalRange[0]), np.array(yellowGoalRange[1]))
-        self.blueGoalRange = (np.array(blueGoalRange[0]), np.array(blueGoalRange[1]))
+        self.yellowGoalRange = (np.array(yellowGoalRange[0]),
+                                np.array(yellowGoalRange[1]))
+        self.blueGoalRange = (np.array(blueGoalRange[0]),
+                              np.array(blueGoalRange[1]))
         self.camHeight = camHeight
 
     def readCam(self):
@@ -28,16 +31,17 @@ class Vision:
         return cv2.inRange(img, self.ballMatchRange[0], self.ballMatchRange[1])
 
     def makeYellowGoalMask(self, img):
-        return cv2.inRange(img, self.yellowGoalRange[0], self.yellowGoalRange[1])
+        return cv2.inRange(img, self.yellowGoalRange[0],
+                           self.yellowGoalRange[1])
 
     def makeBlueGoalMask(self, img):
         return cv2.inRange(img, self.blueGoalRange[0], self.blueGoalRange[1])
-        
-    def findRect(self, mask, minArea: int):
+
+    def findRect(self, mask, areaThresh: int):
         contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL,
                                                cv2.CHAIN_APPROX_NONE)
         for cnt in contours:
-            if cv2.contourArea(cnt) > minArea:
+            if cv2.contourArea(cnt) > areaThresh:
                 peri = cv2.arcLength(cnt, True)
                 approx = cv2.approxPolyDP(cnt, 0.02 * peri, True)
                 return cv2.boundingRect(
