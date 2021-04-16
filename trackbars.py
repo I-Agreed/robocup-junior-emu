c5a4dabfb0914c4c0277dc7acd6c39bec7b77ac7
chickensalt <nsdj.sharma@gmail.com>
2021-04-16 18:19:36 +1000

    make trackbars.py format output better

--- trackbars.py
+++ trackbars.py
@@ -1,59 +1,68 @@
-import cv2 as cv
+import cv2 as cv2
 from cv2.cv2 import VideoCapture
 import numpy as np
 from sys import argv
 
+
 def empty(img):
     pass
 
-def analyse(img):
-    imgHsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-    hueMin = cv.getTrackbarPos("Hue Min", "TrackBars")
-    hueMax = cv.getTrackbarPos("Hue Max", "TrackBars")
-    satMin = cv.getTrackbarPos("Sat Min", "TrackBars")
-    satMax = cv.getTrackbarPos("Sat Max", "TrackBars")
-    valMin = cv.getTrackbarPos("Val Min", "TrackBars")
-    valMax = cv.getTrackbarPos("Val Max", "TrackBars")
+def analyse(img):
+    imgHsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+
+    hueMin = cv2.getTrackbarPos("Hue Min", "TrackBars")
+    hueMax = cv2.getTrackbarPos("Hue Max", "TrackBars")
+    satMin = cv2.getTrackbarPos("Sat Min", "TrackBars")
+    satMax = cv2.getTrackbarPos("Sat Max", "TrackBars")
+    valMin = cv2.getTrackbarPos("Val Min", "TrackBars")
+    valMax = cv2.getTrackbarPos("Val Max", "TrackBars")
 
     minVals = np.array([hueMin, satMin, valMin])
     maxVals = np.array([hueMax, satMax, valMax])
-    mask = cv.inRange(imgHsv, minVals, maxVals)
-    maskColor = cv.bitwise_and(img, img, mask=mask)
+    mask = cv2.inRange(imgHsv, minVals, maxVals)
+    maskColor = cv2.bitwise_and(img, img, mask=mask)
 
-    cv.imshow("Original Image", img)
-    cv.imshow("HSV Image", imgHsv)
-    cv.imshow("Masked Image", mask)
-    cv.imshow("Masked Image with Color", maskColor)
-    
-    if cv.waitKey(1) & 0xFF == ord('q'):
-        cv.destroyAllWindows()
+    cv2.imshow("Original Image", img)
+    cv2.imshow("HSV Image", imgHsv)
+    cv2.imshow("Masked Image", mask)
+    cv2.imshow("Masked Image with Color", maskColor)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
         print("")
+        print("--- RAW VALUES ---")
         print(f"Minimum Hue: {hueMin}")
-        print(f"Maximum Hue: {hueMax}")
         print(f"Minimum Saturation: {satMin}")
-        print(f"Maximum Saturation: {satMax}")
         print(f"Minimum Value: {valMin}")
+        print(f"Maximum Hue: {hueMax}")
+        print(f"Maximum Saturation: {satMax}")
         print(f"Maximum Value: {valMax}")
+        print("")
+        print("--- PYTHON CODE ---")
+        print(f"minVals = np.array([{hueMin}, {satMin}, {valMin}])")
+        print(f"maxVals = np.array([{hueMax}, {satMax}, {valMax}])")
+        print(f"mask = cv2.inrange(img, minVals, maxVals)")
         exit()
 
-cv.namedWindow("TrackBars")
-cv.resizeWindow("TrackBars", 640, 240)
-cv.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)
-cv.createTrackbar("Hue Max", "TrackBars", 170, 179, empty)
-cv.createTrackbar("Sat Min", "TrackBars", 0, 255, empty)
-cv.createTrackbar("Sat Max", "TrackBars", 255, 255, empty)
-cv.createTrackbar("Val Min", "TrackBars", 0, 255, empty)
-cv.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
+
+cv2.namedWindow("TrackBars")
+cv2.resizeWindow("TrackBars", 640, 240)
+cv2.createTrackbar("Hue Min", "TrackBars", 0, 179, empty)
+cv2.createTrackbar("Hue Max", "TrackBars", 170, 179, empty)
+cv2.createTrackbar("Sat Min", "TrackBars", 0, 255, empty)
+cv2.createTrackbar("Sat Max", "TrackBars", 255, 255, empty)
+cv2.createTrackbar("Val Min", "TrackBars", 0, 255, empty)
+cv2.createTrackbar("Val Max", "TrackBars", 255, 255, empty)
 
 print("Hit 'q' to quit!")
 if not len(argv) >= 2:
     cap = VideoCapture(0)
-    cap.set(10, 100)
+    cap.set(cv2.CAP_PROP_BRIGHTNESS, 100)
     while True:
         success, img = cap.read()
         analyse(img)
 else:
     while True:
-        img = cv.imread(argv[1])
+        img = cv2.imread(argv[1])
         analyse(img)
